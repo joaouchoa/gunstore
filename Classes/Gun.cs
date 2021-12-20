b6d1f56585a67a43560b7db1c3b6c9ef42b1a032
João Uchôa <joaodep2@gmail.com>
2021-12-20 11:10:34 -0300

    Aula 02

---
+++ Classes/Gun.cs
@@ -0,0 +1,45 @@
+using gunstore.Enum;
+
+namespace gunstore.Classes
+{
+    public class Gun : Weapon
+    {
+        private string? Fabricante { get; set; }
+        private int Capacidade { get; set; }
+        private string? Modelo { get; set; }
+        private ECalibre Calibre {get; set;}
+        private string? NumeroDeSerie {get; set;}
+
+        public Gun(string fabricante, int id, ECalibre calibre, int capacidade, string modelo, string numeroDeSerie)
+        {
+            this.id = id;   
+            Calibre = calibre;
+            Capacidade = capacidade;
+            Modelo = modelo;
+            NumeroDeSerie = numeroDeSerie;
+            Fabricante = fabricante;
+        }
+
+        public override string ToString()
+        {
+            string retorno = "";
+            retorno += "Modelo: " + this.Modelo + Environment.NewLine;
+            retorno += "Calibre: " + this.Calibre + Environment.NewLine;
+            retorno += "Capacidade: " + this.Capacidade + Environment.NewLine;
+            retorno += "Fabricante: " + this.Fabricante + Environment.NewLine;
+            retorno += "Numero de Série: " + this.NumeroDeSerie + Environment.NewLine;
+
+            return retorno;
+        }
+
+        public string retornaModelo()
+        {
+            return this.Modelo;
+        }
+
+        public int retornaId()
+        {
+            return this.id;
+        }
+    }
+}
---
+++ Classes/Weapon.cs
@@ -0,0 +1,7 @@
+namespace gunstore.Classes
+{
+    public abstract class Weapon
+    {
+        public int id { get; protected set; }
+    }
+}
